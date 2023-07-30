bb1c393b440ba52e0e897b41a4352ccc1ef72f40
Armin joyaian <137637877+Arminjoyaian@users.noreply.github.com>
2023-07-30 11:46:52 +0330

    Delete dim.py

--- dim.py
+++
@@ -1,9 +0,0 @@
-
-print("💓")
-char = input("enter number  : ")
-def diamond(length):
-    for i in range(length):
-        print(((length - i) * "  "), ((i * 2 - 1) * char), ((length - i) * "    "), ((i * 2 - 1) * char))
-    for i in range(length, 0, -1):
-        print(((length - i) * "  "), ((i * 2 - 1) * char), ((length - i) * "    "), ((i * 2 - 1) * char))
-diamond(length)
